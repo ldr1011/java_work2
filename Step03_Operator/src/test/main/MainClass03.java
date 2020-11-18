4c69b4101296bf1fd8fd22ebdfadc2b01035e7f5
Leedongryul <lol.trust10@gmail.com>
2020-11-18 16:04:45 +0900

    Step03_Operator 프로젝트 추가됨

---
+++ Step03_Operator/src/test/main/MainClass03.java
@@ -0,0 +1,18 @@
+package test.main;
+
+public class MainClass03 {
+	public static void main(String[] args) {
+		boolean result1=10==10; //true
+		boolean result2=10!=10; //false
+		boolean result3=10>100; //false
+		boolean result4=10>=10; //true
+		boolean result5=10<100; //true
+		boolean result6=10<=10; //true
+		
+		// String type 변수에 numll 대입하기
+		String name=null;
+		
+		boolean result7=name==null; //true
+		boolean result8=name!=null; //false
+	}
+}
